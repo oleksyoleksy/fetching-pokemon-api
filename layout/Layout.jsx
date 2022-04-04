93d68398d89419a271e75ac11ea80552fb762daf
Marcin Oleksiak <oleksyoleksy@gmail.com>
2022-04-04 19:33:40 +0200

    14--refactoring nav

--- layout/Layout.jsx
+++ layout/Layout.jsx
@@ -4,6 +4,8 @@ import {useEffect} from 'react';
 // YARN
 import {useSnapshot} from 'valtio';
 import {cx, css} from '@emotion/css';
+import {math} from 'polished';
+import {isMobile} from 'react-device-detect';
 // REPO-JS
 import {state} from 'state.js';
 // REPO-JSX
@@ -48,12 +50,70 @@ const Layout = ({children}) => {
 	}, []);
 
 
-
+// width: ${math(`${state.width} - 20`)}px;
 
 
   const ALL = css`
+    header {
+      z-index: 1;
+      position: fixed;
+      top: 0px;
+      left: 0px;
+      width: 100%;
+      padding-top: 20px;
+      background-color: #ffff00;
+      padding-bottom: 20px;
+      nav {
+        position: sticky;
+        width: 600px;
+        margin-left: ${math(`(${state.width} - 600) / 2`)}px;
+      }
+    }
     main {
-      margin-top: 20px;
+      margin-top: 100px;
+      position: relative;
+      width: 600px;
+      margin-left: ${math(`(${state.width} - 600) / 2`)}px;
+      .pokemons-grid {
+        display: grid;
+        grid-template-columns: repeat(3, 200px);
+      }
+      img {
+        width: 200px;
+      }
+      p {
+        text-align: center;
+      }
+    }
+  `;
+
+  const MOB = css`
+    header {
+      nav {
+        width: 100vw;
+        margin-left: 10px;
+        a {
+          font-weight: 500;
+          font-size: 24px;
+        }
+      }
+    }
+    main {
+      width: 100vw;
+      margin-left: 10px;
+      .pokemons-grid {
+        display: grid;
+        grid-template-columns: repeat(3, 32vw);
+        justify-items: center;
+      }
+      img {
+        width: 25vw;
+      }
+      p {
+        text-align: center;
+        font-weight: 500;
+        font-size: 18px;
+      }
     }
   `;
 
@@ -64,6 +124,7 @@ const Layout = ({children}) => {
   return (
     <div lang="pl" id="header-main-footer" className={cx(
       ALL,
+      {[MOB]: isMobile === true},
     )}>
       <Header/>
       <main>{children}</main>
