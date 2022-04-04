36c3a6fcc9cbdc090d448ed9934d4f486fb89238
Marcin Oleksiak <oleksyoleksy@gmail.com>
2022-04-04 17:24:53 +0200

    deleting useless code

--- comps/Nav.jsx
+++ comps/Nav.jsx
@@ -1,86 +1,29 @@
 // NEXT
+import Link from 'next/link';
 // REACT
-import {useEffect} from 'react';
 // YARN
 import {cx, css} from '@emotion/css';
-import {useSnapshot} from 'valtio';
 // REPO-JS
-import {state} from 'state.js';
 // REPO-JSX
-import NavLinks from 'comps/NavLinks';
 // REPO-SCSS
 
 
 const Nav = () => {
 
-  // with useSnapshot() hook, I can see recent valtio-state values 
-  // in React DevTools > MyApp > Layout > hooks > Snapshot > Ref > ...
-  useSnapshot(state);
-
-
-
-
-
-  useEffect(() => {
-    for (let i = 0; i < state.how_many_links_in_navbar; i++) {
-      //console.log(`a-tag-${i+1}`);
-      //console.log(document.querySelector(`.a-tag-${i+1}`).innerHTML.toUpperCase());
-      //console.log(document.querySelector(`.a-tag-${i+1}`).offsetWidth);
-      state.all_links_width_combined = 
-        state.all_links_width_combined + 
-        document.querySelector(`.a-tag-${i+1}`).offsetWidth;
-    }
-    //console.log(state.all_links_width_combined);
-	}, []);
-
-  state.space_between_two_links = 
-    (state.width - state.all_links_width_combined) / 
-    (state.how_many_links_in_navbar + 1);
-  //console.log(state.space_between_two_links);
-
-
-
-
-
-  const switch_to_hamburger = css`
-    a {
-      background-color: rgba(255, 255, 0, 1.0)
-    }
-  `;
-
   const ALL = css`
-    width: 100vw;
-    height: 50px;
-    background-color: rgba(224, 224, 224, 1.0);
-    //background-color: rgba(224, 224, 224, 0.5);
-    //background-color: red;
-    ul {
-      height: 50px;
-      display: flex;
-      flex-direction: row;
-      justify-content: space-evenly; // horizontally
-      align-items: center; // vertically
-    }
     a {
-      text-transform: uppercase;
       text-decoration: none;
     }
-    .active,
     a:hover {
-      text-decoration: underline;
+      text-decoration: underline rgb(128, 128, 128);
     }
   `;
-
-
-
-
 
   return (
     <nav className={cx(
       ALL,
-      {[switch_to_hamburger]: state.space_between_two_links <= 25},
     )}>
-      <NavLinks/>
+      <Link href="/"><a>homepage</a></Link>
     </nav>
   );
 }
--- layout/Layout.jsx
+++ layout/Layout.jsx
@@ -6,15 +6,8 @@ import {useSnapshot} from 'valtio';
 import {cx, css} from '@emotion/css';
 // REPO-JS
 import {state} from 'state.js';
-import {
-  desktop_small, desktop_medium, desktop_large,
-  laptop_small, laptop_medium, laptop_large,
-  tablet_small, tablet_medium, tablet_large,
-  phone_small, phone_medium, phone_large,
-} from 'breakpoints.js';
 // REPO-JSX
 import Header from 'layout/Header';
-import Footer from 'layout/Footer';
 // REPO-SCSS
 
 
@@ -52,278 +45,17 @@ const Layout = ({children}) => {
     // after every and any viewport's resize
     // ==========================================================
 
-
-
-
-
-    // ============================================
-    // shifting <main> when hiding/showing <header>
-
-    const mainSelector = document.querySelector("main");
-
-    // reading current `scrollY` value dynamically
-    // (state.vertical_scroll is dynamically updated in Header.jsx)
-    let last_vertical_scroll = state.vertical_scroll;
-
-    // if scrolling down, <main> must be shifted up
-    const scrolling_watcher = () => {
-      if (last_vertical_scroll < window.scrollY) {
-        mainSelector.classList.add("main-is-shifted");
-      } else {
-        mainSelector.classList.remove("main-is-shifted");
-      }
-    };
-
-    window.addEventListener('scroll', scrolling_watcher);
-
-    // shifting <main> when hiding/showing <header>
-    // ============================================
-
 	}, []);
 
 
 
 
 
-
-
-
-
-
-  // --------------------------------------------
-  //    0 - 8888
   const ALL = css`
-
-    // -----------------------------------------
-    // hide/show <header> when scrolling up/down
-    // JS-logic for this is in ~/layout/Header.jsx
-    header {
-      position: fixed;
-      top: 0px;
-      left: 0px;
-      transition: transform 0.4s;
-    }
-    header.header-is-hidden {
-      transform: translateY(-150px); // 3 * height of Nav.jsx
-    }
     main {
-      padding-top: 50px; // because in 'comps/Nav.jsx' height: 50px; (for ALL <nav> and <ul>)
-      padding-bottom: 25px;
-    }
-    // hide/show <header> when scrolling up/down
-    // -----------------------------------------
-
-
-
-
-    // --------------------------------------------
-    // shifting <main> when hiding/showing <header>
-    // JS-logic for this is here, in ~/layout/Layout.jsx
-    main {
-      transition-property: padding-top;
-      transition-duration: 0.3s;
-    }
-    main.main-is-shifted {
-      padding-top: 20px;
-    }
-    // shifting <main> when hiding/showing <header>
-    // --------------------------------------------
-
-
-
-
-
-    main {
-      margin-top: 0px;
-      margin-bottom: 0px;
-      border-style: solid;
-      border-width: 0px;
-      background-color: red;
-      .jsx-content {
-        margin-top: 20px;
-      }
+      margin-top: 20px;
     }
   `;
-
-
-
-  // -----------
-  // 1801 - 8888
-  const MAX = css`
-    main {
-      border-color: rgba(0, 0, 0, 1.0);
-      background-color: rgba(255, 255, 255, 1.0);
-    }
-  `;
-
-
-
-
-
-
-
-
-
-
-  // --------------------------------------------
-  // 1201 - 1800
-  const DESKTOP = css`
-    main {
-      border-color: rgba(0, 0, 0, 0.25);
-    }
-  `;
-
-  // -----------
-  // 1601 - 1800
-  const DESKTOP_LARGE = css`
-    main {
-      background-color: rgba(0, 0, 0, 0.2);
-    }
-  `
-
-  // -----------
-  // 1401 - 1600
-  const DESKTOP_MEDIUM = css`
-    main {
-      background-color: rgba(0, 0, 0, 0.25);
-    }
-  `;
-
-  // -----------
-  // 1201 - 1400
-  const DESKTOP_SMALL = css`
-    main {
-      background-color: rgba(0, 0, 0, 0.3);
-    }
-  `;
-
-
-
-
-
-
-
-
-
-
-  // --------------------------------------------
-  //  901 - 1200
-  const LAPTOP = css`
-    main {
-      border-color: rgba(0, 255, 0, 0.5);
-    }
-  `;
-
-  // -----------
-  // 1101 - 1200
-  const LAPTOP_LARGE = css`
-    main {
-      background-color: rgba(255, 255, 0, 0.2);
-    }
-  `;
-
-  // -----------
-  // 1001 - 1100
-  const LAPTOP_MEDIUM = css`
-    main {
-      background-color: rgba(255, 255, 0, 0.4);
-    }
-  `;
-
-  // -----------
-  //  901 - 1000
-  const LAPTOP_SMALL = css`
-    main {
-      background-color: rgba(255, 255, 0, 0.7);
-    }
-  `;
-
-
-
-
-
-
-
-
-
-
-  // --------------------------------------------
-  //  601 -  900
-  const TABLET = css`
-    main {
-      border-color: rgba(255, 0, 0, 0.5);
-    }
-  `;
-
-  // -----------
-  //  801 -  900
-  const TABLET_LARGE = css`
-    main {
-      background-color: rgba(255, 0, 255, 0.2);
-    }
-  `;
-
-  // -----------
-  //  701 -  800
-  const TABLET_MEDIUM = css`
-    main {
-      background-color: rgba(255, 0, 255, 0.3);
-    }
-  `;
-
-  // -----------
-  //  601 -  700
-  const TABLET_SMALL = css`
-    main {
-      background-color: rgba(255, 0, 255, 0.4);
-    }
-  `;
-
-
-
-
-
-
-
-
-
-
-  // --------------------------------------------
-  //    0 -  600
-  const PHONE = css`
-    main {
-      border-color: rgba(0, 0, 255, 0.5);
-    }
-  `;
-
-  // -----------
-  //  501 -  600
-  const PHONE_LARGE = css`
-    main {
-      background-color: rgba(0, 255, 255, 0.2);
-    }
-  `;
-
-  // -----------
-  //  401 -  500
-  const PHONE_MEDIUM = css`
-    main {
-      background-color: rgba(0, 255, 255, 0.4);
-    }
-  `;
-
-  // -----------
-  //    0 -  400
-  const PHONE_SMALL = css`
-    main {
-      background-color: rgba(0, 255, 255, 0.6);
-    }
-  `;
-
-
-
-
-
 
 
 
@@ -332,31 +64,9 @@ const Layout = ({children}) => {
   return (
     <div lang="pl" id="header-main-footer" className={cx(
       ALL,
-      {[MAX]:            state.width >  desktop_large},
-
-      {[DESKTOP]:        state.width >  laptop_large   && state.width <= desktop_large},
-      {[DESKTOP_LARGE]:  state.width >  desktop_medium && state.width <= desktop_large},
-      {[DESKTOP_MEDIUM]: state.width >  desktop_small  && state.width <= desktop_medium},
-      {[DESKTOP_SMALL]:  state.width >  laptop_large   && state.width <= desktop_small},
-
-      {[LAPTOP]:         state.width >  tablet_large   && state.width <= laptop_large},
-      {[LAPTOP_LARGE]:   state.width >  laptop_medium  && state.width <= laptop_large},
-      {[LAPTOP_MEDIUM]:  state.width >  laptop_small   && state.width <= laptop_medium},
-      {[LAPTOP_SMALL]:   state.width >  tablet_large   && state.width <= laptop_small},
-
-      {[TABLET]:         state.width >  phone_large    && state.width <= tablet_large},
-      {[TABLET_LARGE]:   state.width >  tablet_medium  && state.width <= tablet_large},
-      {[TABLET_MEDIUM]:  state.width >  tablet_small   && state.width <= tablet_medium},
-      {[TABLET_SMALL]:   state.width >  phone_large    && state.width <= tablet_small},
-
-      {[PHONE]:          state.width <= phone_large},
-      {[PHONE_LARGE]:    state.width >  phone_medium   && state.width <= phone_large},
-      {[PHONE_MEDIUM]:   state.width >  phone_small    && state.width <= phone_medium},
-      {[PHONE_SMALL]:    state.width <= phone_small},
     )}>
       <Header/>
       <main>{children}</main>
-      <Footer/>
     </div>
   );
 }
