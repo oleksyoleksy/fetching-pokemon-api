dc0b9d7e0e08dca3233a5192ee786344d3254f98
Marcin Oleksiak <oleksyoleksy@gmail.com>
2022-04-08 14:16:47 +0200

    25--HTML for single pokemon

--- content/load_more/Name.jsx
+++ content/load_more/Name.jsx
@@ -1,15 +1,26 @@
 // NEXT
+import Link from 'next/link';
 // REACT
 //import {useEffect} from 'react';
 // YARN
 import {cx, css} from '@emotion/css';
 //import { v4 as uuidv4 } from 'uuid';
+import {useSnapshot} from 'valtio';
 // REPO-JS
+import {state} from 'state.js';
 // REPO-JSX
 // REPO-SCSS
 
 
 const Name = ({data}) => {
+
+  // with useSnapshot() hook, I can see recent valtio-state values 
+  // in React DevTools > MyApp > Layout > hooks > Snapshot > Ref > ...
+  useSnapshot(state);
+
+  const onClickFx = () => {
+    state.limit = state.limit + 3;
+  };
 
   /* useEffect(() => {
     console.log(data);
@@ -33,6 +44,21 @@ const Name = ({data}) => {
         <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${data.id}.png`} alt={data.id} />
         <h2>id: {data.id}</h2>
         <h3>weight: {data.weight}</h3>
+
+        <div id="loadmorebutton-jsx">
+          <div id="loadmorebutton-jsx-content" className="jsx-content">
+            <Link href={
+              `/${state.limit}`
+              //state.limit <= 6 && `/${state.limit + 3}` ||
+              //state.limit > 6 && `/9`
+            }>
+              <button onClick={onClickFx} className="link-like-button">
+                go back
+              </button>
+            </Link>
+          </div>
+        </div>
+
       </div>
     </div>
   );
