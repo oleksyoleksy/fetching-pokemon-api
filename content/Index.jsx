6e6827c2bfc54157e8399aae1a078c1b3a957bb6
Marcin Oleksiak <oleksyoleksy@gmail.com>
2022-04-10 13:54:17 +0200

    27c--content/Index.jsx and content/LoadMore.jsx, ALL and PHONE

--- content/Index.jsx
+++ content/Index.jsx
@@ -2,9 +2,17 @@
 import Link from 'next/link';
 // REACT
 // YARN
+import {isMobile} from 'react-device-detect';
+import {cx, css} from '@emotion/css';
 import { v4 as uuidv4 } from 'uuid';
 import {useSnapshot} from 'valtio';
 // REPO-JS
+import {
+  desktop_small, desktop_medium, desktop_large,
+  laptop_small, laptop_medium, laptop_large,
+  tablet_small, tablet_medium, tablet_large,
+  phone_small, phone_medium, phone_large,
+} from 'breakpoints.js';
 import {state} from 'state.js';
 // REPO-JSX
 import LoadMoreButton from 'comps/LoadMoreButton';
@@ -24,9 +32,42 @@ const Index = ({pokemons_array}) => {
   state.limit = 3;
   useSnapshot(state);
 
+  const ALL = css`
+    a {
+      font-size: 22px;
+    }
+  `;
+  const MAX = css``;
+  const DESKTOP = css``;
+  const LAPTOP = css``;
+  const TABLET = css``;
+  const PHONE = css`
+    h1 {
+      text-align: center;
+    }
+    button {
+      margin: 0 auto;
+    }
+  `;
+  const PHONE_LARGE = css``;
+  const PHONE_MEDIUM = css``;
+  const PHONE_SMALL = css``;
+
   return (
     <div id="index-jsx">
-      <div id="index-jsx-content" className="jsx-content">
+      <div id="index-jsx-content" className={cx(
+        "jsx-content",
+        ALL,
+        {[MAX]:            state.width >  desktop_large},
+        {[DESKTOP]:        state.width >  laptop_large   && state.width <= desktop_large},
+        {[LAPTOP]:         state.width >  tablet_large   && state.width <= laptop_large},
+        {[TABLET]:         state.width >  phone_large    && state.width <= tablet_large},
+        {[PHONE]:          state.width <= phone_large && isMobile === true},
+        {[PHONE_LARGE]:    state.width >  phone_medium   && state.width <= phone_large},
+        {[PHONE_MEDIUM]:   state.width >  phone_small    && state.width <= phone_medium},
+        {[PHONE_SMALL]:    state.width <= phone_small},
+        //{[MOB]: isMobile === true},
+      )}>
 
         <h1>Pokémons:</h1>
 
--- content/LoadMore.jsx
+++ content/LoadMore.jsx
@@ -3,9 +3,17 @@ import { useRouter } from "next/router";
 import Link from 'next/link';
 // REACT
 // YARN
+import {isMobile} from 'react-device-detect';
+import {cx, css} from '@emotion/css';
 import {useSnapshot} from 'valtio';
 import { v4 as uuidv4 } from 'uuid';
 // REPO-JS
+import {
+  desktop_small, desktop_medium, desktop_large,
+  laptop_small, laptop_medium, laptop_large,
+  tablet_small, tablet_medium, tablet_large,
+  phone_small, phone_medium, phone_large,
+} from 'breakpoints.js';
 import {state} from 'state.js';
 // REPO-JSX
 import LoadMoreButton from 'comps/LoadMoreButton';
@@ -28,9 +36,42 @@ const LoadMore = ({pokemons_array}) => {
 
   useSnapshot(state);
 
+  const ALL = css`
+    a {
+      font-size: 22px;
+    }
+  `;
+  const MAX = css``;
+  const DESKTOP = css``;
+  const LAPTOP = css``;
+  const TABLET = css``;
+  const PHONE = css`
+    h1 {
+      text-align: center;
+    }
+    button {
+      margin: 0 auto;
+    }
+  `;
+  const PHONE_LARGE = css``;
+  const PHONE_MEDIUM = css``;
+  const PHONE_SMALL = css``;
+
   return (
     <div id="loadmore-jsx">
-      <div id="loadmore-jsx-content" className="jsx-content">
+      <div id="loadmore-jsx-content" className={cx(
+        "jsx-content",
+        ALL,
+        {[MAX]:            state.width >  desktop_large},
+        {[DESKTOP]:        state.width >  laptop_large   && state.width <= desktop_large},
+        {[LAPTOP]:         state.width >  tablet_large   && state.width <= laptop_large},
+        {[TABLET]:         state.width >  phone_large    && state.width <= tablet_large},
+        {[PHONE]:          state.width <= phone_large && isMobile === true},
+        {[PHONE_LARGE]:    state.width >  phone_medium   && state.width <= phone_large},
+        {[PHONE_MEDIUM]:   state.width >  phone_small    && state.width <= phone_medium},
+        {[PHONE_SMALL]:    state.width <= phone_small},
+        //{[MOB]: isMobile === true},
+      )}>
         
         <h1>Pokémons:</h1>
 
